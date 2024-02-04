3058dbe927338fa55c57703fde8699f2bde08fdf
Lev Yuvenskiy <levayu22@gmail.com>
2024-02-04 17:08:41 +0300

    Change scroll-to-top button

--- static/js/scroll-to-top.js
+++ static/js/scroll-to-top.js
@@ -6,8 +6,17 @@ btn.addEventListener('click', () => window.scrollTo({
   behavior: 'smooth',
 }));
 
-if (article.offsetHeight > window.innerHeight) {
-  btn.style.visibility = "visible";
-} else {
-  btn.style.visibility = "hidden";
+function getVerticalScrollPercentage(elm) {
+  var p = elm.parentNode
+  return (elm.scrollTop || p.scrollTop) / (p.scrollHeight - p.clientHeight) * 100
+}
+
+document.onscroll = function() {
+  if (getVerticalScrollPercentage(document.body) > 0) {
+    btn.style.visibility = "visible";
+    btn.style.opacity = 1;
+  } else {
+    btn.style.visibility = "hidden";
+    btn.style.opacity = 0;
+  }
 }
